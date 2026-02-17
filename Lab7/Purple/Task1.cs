9454f0b8ad457f9ddba78a4d62d3af4a68717379
Schroge <158596266+AnatoliiPashtov@users.noreply.github.com>
2026-02-17 14:17:02 +0300

    Update Task1.cs

--- Lab7/Purple/Task1.cs
+++ Lab7/Purple/Task1.cs
@@ -1,135 +1,105 @@
-using System.Security.Cryptography.X509Certificates;
+using System.Runtime.InteropServices;
+using static Lab7.Purple.Task1;
 
 namespace Lab7.Purple
 {
-    public class Task1
+  public class Task1
+  {
+    public struct Participant
     {
-        public struct Participant
-        {
-            private string _name;
-            private string _surname;
-            private double[] _coefs;
-            private int[,] _marks;
+      // Поля
+      private string _name;
+      private string _surname;
+      private double[] _coefs;
+      private int[,] _marks;
 
-            public string Name => _name;
-            public string Surname => _surname;
-            public double[] Coefs
-            {
-                get
-                {
-                    double[] result = new double[4];
-                    for (int i = 0; i<result.Length; i++)
-                    {
-                        result[i] = _coefs[i];
-                    }
-                    return result;
-                }
-            }
-            public int[,] Marks
-            {
-                get
-                {
-                    int[,] result = new int[4, 7];
-                    for (int i = 0; i < result.GetLength(0); i++)
-                    {
-                        for (int j = 0; j < result.GetLength(1); j++)
-                        {
-                            result[i,j] = _marks[i,j];
-                        }
-                    }
-                    return result;
-                }
-            }
-
-            public double TotalScore
-            {
-                get
-                {
-                    if (_marks == null || _marks.Length == 0) return 0;
-                    double sumTotal = 0;
-                    for (int k = 0; k < _coefs.Length; k++)
-                    {
-                        int sum = 0;
-                        int min = _marks[k, 0];
-                        int minIndex = 0;
-                        int max = _marks[k, 0];
-                        int maxIndex = 0;
-                        // Находим минимальную и максимальную оценку
-                        for (int j = 0; j < _marks.GetLength(1); j++)
-                        {
-                            if (_marks[k, j] < min)
-                            {
-                                min = _marks[k, j];
-                                minIndex = j;
-                            }
-                            if (_marks[k, j] > max)
-                            {
-                                max = _marks[k, j];
-                                maxIndex = j;
-                            }
-                        }
-                        // Находим сумму оценок без минимальной и максимальной
-                        for (int j = 0; j < _marks.GetLength(1); j++)
-                        {
-                            if (j != minIndex && j != maxIndex)
-                            {
-                                sum += _marks[k, j];
-                            }
-                        }
-                        sumTotal += sum * _coefs[k]; // Оценка за все прыжки спортсмена
-                    }
-                    return sumTotal;
-                }
-                
-            }
-
-            public Participant(string name, string surname)
-            {
-                _name = name;
-                _surname = surname;
-                _coefs = new double[4] { 2.5, 2.5, 2.5, 2.5 };
-                _marks = new int[,]{
-                    { 0, 0, 0, 0, 0, 0, 0},
-                    { 0, 0, 0, 0, 0, 0, 0},
-                    { 0, 0, 0, 0, 0, 0, 0},
-                    { 0, 0, 0, 0, 0, 0, 0}
-                };
-            }
-            public void SetCriterias(double[] coefs)
-            {
-                if (coefs == null || coefs.Length == 0) return;
-                _coefs = new double[coefs.Length];
-                for (int i = 0; i < coefs.Length; i++)
-                {
-                    _coefs[i] = coefs[i];
-                }
-            }
-            int count = 0;
-            public void Jump(int[] marks)
-            {
-                if (marks == null || marks.Length == 0) return;
-                for (int j = 0; j < marks.Length; j++)
-                {
-                    _marks[count, j] = marks[j];
-                }
-                count++;
-            }
-            public static void Sort(Participant[] array)
-            {
-                for (int i = 0; i<array.Length; i++)
-                {
-                    for (int j = i; j < array.Length; j++) {
-                        if (array[i].TotalScore < array[j].TotalScore)
-                        {
-                            (array[i], array[j]) = (array[j], array[i]);
-                        }
-                    }
-                }
-            }
-            public void Print()
-            {
-                Console.WriteLine($"{_name} {_surname} {_marks} {_coefs}");
-            }
+      // Свойства
+      public string Name => _name;
+      public string Surname => _surname;
+      public double[] Coefs
+      {
+        get {
+          double[] answer = new double[4];
+          for (int i = 0; i < _coefs.Length; i++)
+            answer[i] = _coefs[i];
+          return answer;
         }
+      }
+      public int[,] Marks
+      {
+        get
+        {
+          int[,] answer = new int[4, 7];
+          for (int i = 0; i < answer.GetLength(0); i++)
+            for (int j = 0; j < answer.GetLength(1); j++)
+              answer[i, j] = _marks[i, j];
+          return answer;
+        }
+      }
+      public double TotalScore
+      {
+        get
+        {
+          double answer = 0;
+          for (int i = 0; i < _marks.GetLength(0); i++)
+          {
+            double S = 0, mx = int.MinValue, mn = int.MaxValue;
+            for (int j = 0; j < _marks.GetLength(1); j++)
+            {
+              (mx, mn) = (Math.Max(_marks[i, j], mx), Math.Min(_marks[i, j], mn));
+              S += _marks[i, j];
+            }
+            S = S - (mx + mn);
+            S = S * _coefs[i];
+            answer += S;
+          }
+          return answer;
+        }
+      }
+
+      // Методы
+      public Participant(string name, string surname)
+      {
+        _name = name;
+        _surname = surname;
+        _coefs = new double[4] { 2.5, 2.5, 2.5, 2.5 };
+        _marks = new int[4, 7];
+        
+      }
+      public void SetCriterias(double[] coefs)
+      {
+        for (int i = 0; i < coefs.Length; i++)
+          _coefs[i] = coefs[i];
+      }
+      int counter = 0;
+      public void Jump(int[] marks)
+      {
+        for (int i = 0; i < marks.Length; i++)
+          _marks[counter, i] = marks[i];
+        counter++;
+        if (counter >= 4) counter = 0;
+      }
+      public static void Sort(Participant[] array)
+      {
+        for (int i = 0; i < array.Length; i++)
+          for (int j = 0; j < array.Length - i - 1; j++)
+            if (array[j].TotalScore < array[j + 1].TotalScore)
+              (array[j], array[j + 1]) = (array[j + 1], array[j]);
+      }
+      public void Print()
+      {
+        Console.Write($"Name:{_name}  Surname:{_surname}\nCoefs:");
+        for (int i = 0; i < _coefs.Length; i++)
+          Console.Write(_coefs[i] + " ");
+        Console.Write("\nMarks:");
+        for (int i = 0; i < _marks.GetLength(0); i++)
+        {
+          for (int j = 0; j < _marks.GetLength(1); j++)
+            Console.Write(_marks[i, j] + " ");
+          Console.WriteLine();
+        }
+        Console.WriteLine();
+      }
     }
+  }
 }
